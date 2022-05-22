5e121d8bd7bed0772e512cefaae86d533e90f723
Amal K G <amalkg80333@gmail.com>
2022-05-22 18:30:30 +0530

    final check

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  trendingMovies:any = ""
+  theatreMovies:any = ""
+  popularMovies:any = ""
+
+
+  constructor(private http:HttpClient) { }
+
+  ngOnInit(): void {
+    this.getTrendingMovies()
+    this.getTheatreMovies()
+    this.getPopularMovies()
+  }
+
+  getTrendingMovies(){
+    this.http.get('http://localhost:4200/assets/data/trending-movies.json')
+    .subscribe((movies)=>{
+      this.trendingMovies = movies
+      console.log(movies);
+      
+
+    })
+    
+  }
+
+  getTheatreMovies(){
+    this.http.get('http://localhost:4200/assets/data/theatre-movies.json')
+    .subscribe((movies)=>{
+      this.theatreMovies = movies
+      console.log(movies);
+      
+
+    })
+
+  }
+
+  getPopularMovies(){
+    this.http.get('http://localhost:4200/assets/data/popular-movies.json')
+    .subscribe((movies)=>{
+      this.popularMovies = movies
+      console.log(movies);
+      
+
+    })
+
+  }
+
+
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  username: any = ""
+  password: any = ""
+  errorMsg: any = ""
+
+  constructor(private auth: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    if (this.username.trim().length === 0) {
+      this.errorMsg = "username required";
+    }
+    else if (this.password.trim().length === 0) {
+      this.errorMsg = "password required";
+    }
+    else if (this.password.trim().length < 4) {
+      this.errorMsg = "Minimum 4 charcter required";
+    }
+    else {
+      this.errorMsg = ""
+      let res = this.auth.login(this.username, this.password);
+      if (res === 200) {
+        this.router.navigate(['home']);
+      }
+      if (res === 403) {
+
+        this.errorMsg = "Invalid Credentials" ;
+
+      }
+    }
+  }
+}
+
+
+
+
+
